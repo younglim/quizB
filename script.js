7675ca88d2f12963bed44aea3be22967e99be9c2
KCas <78678795+CKodes@users.noreply.github.com>
2022-08-29 16:01:16 +0800

    rename game.html and game.js to gameA for both, updated src and href

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function websiteVisits(response) {
   document.querySelector('#visitNo').textContent = response.value;
 
   response.value % 2 == 0
-    ? (document.getElementById('play').href = 'game.html')
+    ? (document.getElementById('play').href = 'gameA.html')
     : (document.getElementById('play').href = 'gameB.html');
 }
