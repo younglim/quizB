9cc362f0960712331ee94c31420ad5ad6e89eaae
younglim <younglim@users.noreply.github.com>
2023-05-11 09:32:32 +0800

    Fix quiz

--- end.js
+++ end.js
@@ -10,7 +10,7 @@ const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
-var scoreInput = document.querySelector('input[name="data[Score]"]');
+var scoreInput = document.querySelector('input[id="playerscore"]');
 scoreInput.value = finalScore.innerText;
 
 username.addEventListener('keyup', () => {
